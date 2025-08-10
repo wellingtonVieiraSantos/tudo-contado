bc706ea4b591ac9c35ba191a927e2ce25a176138
Wellington Vieira <wtvsantos@gmail.com>
2025-08-10 16:05:25 -0300

    fix:prisma client error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,6 +2,23 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL
+      }
+    },
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['query', 'error', 'warn']
+        : ['error'],
+    errorFormat: 'minimal'
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+process.on('beforeExit', async () => {
+  await prisma.$disconnect()
+})
