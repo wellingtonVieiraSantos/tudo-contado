ccc87a83f718300b5fe4e0f393904bed78b53d2f
Wellington Vieira <wtvsantos@gmail.com>
2025-08-10 16:40:50 -0300

    fix: error prisma/vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,23 +2,6 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL
-      }
-    },
-    log:
-      process.env.NODE_ENV === 'development'
-        ? ['query', 'error', 'warn']
-        : ['error'],
-    errorFormat: 'minimal'
-  })
+export const prisma = globalForPrisma.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-process.on('beforeExit', async () => {
-  await prisma.$disconnect()
-})
