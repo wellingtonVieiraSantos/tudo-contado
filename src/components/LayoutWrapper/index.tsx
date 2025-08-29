9c17fdb114d546271fa0af3532ffe99c0a827407
Wellington Vieira <wtvsantos@gmail.com>
2025-08-29 10:12:52 -0300

    style: change sideBar to fixed

--- src/components/LayoutWrapper/index.tsx
+++ src/components/LayoutWrapper/index.tsx
@@ -23,7 +23,7 @@ export default function LayoutWrapper({ children }: LayoutWrapperProps) {
     <QueryClientProvider client={queryClient}>
       <div className='h-screen flex'>
         {showSidebar && <SideBar />}
-        <main className='size-full'>{children}</main>
+        <main className='ml-0 lg:ml-40 xl:ml-60 size-full'>{children}</main>
       </div>
     </QueryClientProvider>
   )
