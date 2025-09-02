2f49f545ec939d570a080b3f59389188c6698401
Wellington Vieira <wtvsantos@gmail.com>
2025-09-02 10:09:53 -0300

    style: adjust responsiviness in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
       <div className='flex flex-col lg:flex-row h-full'>
         <Card className='flex-1 border-none text-center lg:text-left bg-transparent size-full p-3 lg:p-8 grid md:place-content-center md:gap-12'>
           <CardHeader className='gap-3 md:gap-12'>
-            <CardTitle className='text-4xl md:text-8xl tracking-wide lg:-mt-20'>
+            <CardTitle className='text-4xl sm:text-7xl 2xl:text-8xl tracking-wide lg:-mt-20'>
               Tome o{' '}
               <span className='bg-gradient-to-r from-button to-badge text-transparent bg-clip-text'>
                 Controle
@@ -61,7 +61,7 @@ export default function Home() {
             <Link href={'/registro'}>
               <Button
                 size='lg'
-                className='h-10 lg:h-13 lg:px-30 lg:rounded-full text-xl w-full lg:w-auto'
+                className='h-10 lg:h-13 lg:px-30 lg:rounded-full md:text-xl w-full lg:w-auto'
               >
                 Começar
                 <ArrowUpRight />
@@ -82,7 +82,7 @@ export default function Home() {
             alt='celular com imagem da rota ganhos'
             width={669}
             height={1280}
-            className='w-full lg:hidden xl:flex size-180 object-contain absolute lg:-left-35 -top-12 md:top-20 drop-shadow-lg drop-shadow-foreground-secondary '
+            className='w-full lg:hidden xl:flex size-180 object-contain absolute lg:-left-35 -top-12 lg:top-20 drop-shadow-lg drop-shadow-foreground-secondary '
           />
         </div>
       </div>
