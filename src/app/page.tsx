d6a69e35095056e97af7e08859b6a0cb4a627d0c
Wellington Vieira <wtvsantos@gmail.com>
2026-02-23 17:22:00 -0300

    refactor: change landing page and add new img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-'use client'
 import { Button } from '@/components/ui/Button'
 import {
   Card,
@@ -7,90 +6,70 @@ import {
   CardHeader,
   CardTitle
 } from '@/components/ui/Card'
-import { useSession } from 'next-auth/react'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
-import { useEffect } from 'react'
 import Image from 'next/image'
-import { ArrowUpRight } from 'lucide-react'
+import { ArrowUpRight, LogIn } from 'lucide-react'
 
 export default function Home() {
-  const { data: session } = useSession()
-  const router = useRouter()
-  useEffect(() => {
-    if (session) router.push('/painel')
-  }, [session, router])
-
   return (
-    <div className=' h-screen w-full overflow-hidden bg-radial to-background from-border'>
-      <div className='w-full flex justify-between items-center p-3 md:p-4'>
+    <>
+      <nav className='w-full flex justify-between md:justify-around items-center p-3 md:p-4 fixed top-0 z-20 bg-card'>
         <Image
           src={'/logo.png'}
           alt='logo image'
           width={192}
           height={192}
-          className='w-10 md:w-12'
+          className='size-10 xl:size-15'
         />
-        <ul className='hidden flex-1 md:flex justify-center gap-8'>
+        <ul className='hidden md:flex justify-center gap-8'>
           <li>Home</li>
           <li>Sobre</li>
           <li>FAQ</li>
           <li>Contato</li>
         </ul>
         <Link href={'/login'}>
-          <Button className='lg:px-5 lg:rounded-full'>Entrar</Button>
+          <Button className='px-6 md:px-12'>
+            Entrar
+            <LogIn />
+          </Button>
         </Link>
-      </div>
-      <div className='flex flex-col lg:flex-row h-full'>
-        <Card className='flex-1 border-none text-center lg:text-left bg-transparent size-full p-3 lg:p-8 grid md:place-content-center md:gap-12'>
-          <CardHeader className='gap-3 md:gap-12'>
-            <CardTitle className='text-4xl sm:text-7xl 2xl:text-8xl tracking-wide lg:-mt-20'>
+      </nav>
+
+      <section className='h-full flex flex-col lg:flex-row p-4 lg:p-8 bg-card lg:bg-radial from-disabled via-card to-background'>
+        <Card className='flex flex-col justify-center items-center flex-1 p-3 bg-transparent border-none gap-8'>
+          <CardHeader className='text-center text-6xl lg:text-8xl gap-3 lg:gap-8'>
+            <CardTitle>
               Tome o{' '}
               <span className='bg-gradient-to-r from-button to-badge text-transparent bg-clip-text'>
                 Controle
               </span>{' '}
               de suas finanças
             </CardTitle>
-            <CardDescription className='text-base md:text-2xl'>
-              Monitore seus gastos, controle seus rendimentos e gerencie{' '}
-              <span className='hidden lg:inline'>
-                de forma simple e intuitiva
-              </span>{' '}
-              o seu dinheiro{' '}
-              <span className='hidden lg:inline'>
-                com esse aplicativo web totalmente gratuito.
-              </span>
+            <CardDescription className='text-lg lg:text-xl leading-relaxed'>
+              Monitore seus gastos, controle seus rendimentos e gerencie de
+              forma simple e intuitiva o seu dinheiro de qualquer lugar, de
+              forma online e totalmente gratuito.
             </CardDescription>
           </CardHeader>
-          <CardContent className='w-full flex'>
+          <CardContent className='w-full flex items-center'>
             <Link href={'/registro'}>
-              <Button
-                size='lg'
-                className='h-9 lg:h-13 lg:px-30 lg:rounded-full md:text-xl w-full lg:w-auto'
-              >
-                Começar
+              <Button size='lg' className='lg:px-30 lg:text-xl w-full'>
+                Criar Conta
                 <ArrowUpRight />
               </Button>
             </Link>
           </CardContent>
         </Card>
-        <div className='h-full relative flex items-center flex-1'>
+
+        <div className='hidden lg:flex items-center justify-center flex-1 drop-shadow-xl drop-shadow-foreground-secondary overflow-hidden'>
           <Image
-            src={'/mobile-painel.png'}
+            src={'/devices.png'}
             alt='celular com imagem da rota painel'
-            width={1152}
-            height={896}
-            className='hidden lg:flex size-200 object-cover z-10 xl:translate-x-55 drop-shadow-lg drop-shadow-foreground-secondary'
-          />
-          <Image
-            src={'/mobile-ganhos.png'}
-            alt='celular com imagem da rota ganhos'
-            width={669}
-            height={1280}
-            className='w-full lg:hidden xl:flex size-180 object-contain absolute lg:-left-35 -top-12 lg:top-20 drop-shadow-lg drop-shadow-foreground-secondary '
+            width={2000}
+            height={1219}
           />
         </div>
-      </div>
-    </div>
+      </section>
+    </>
   )
 }
